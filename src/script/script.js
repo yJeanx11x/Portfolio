46bef6fcbc5dc048aee020f4d7e80427068a4d77
Jean Lucas <j3anlucas010@gmail.com>
2024-10-11 11:01:04 -0300

    melhorias na parte de contatos é retiragem da foto do inicio

--- src/script/script.js
+++ src/script/script.js
@@ -4,9 +4,11 @@ const box=document.querySelector('.box-contato')
 contato.addEventListener('click',()=>{
 if(box.style.opacity == '1'){
         box.style.opacity='0'
+        contato.classList.remove('preto')
 }
 else{
         box.style.opacity='1'
+        contato.classList.add('preto')
 
 }
 })
